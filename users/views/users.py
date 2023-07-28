808a84f8b790805f990736e3967b9fdc06c0d238
Jahongir Ibragimov <118691897+ChogirmaliYigit@users.noreply.github.com>
2023-07-28 11:21:53 +0500

    pinned chat view

--- users/views/users.py
+++ users/views/users.py
@@ -6,7 +6,7 @@ from rest_framework import views
 from rest_framework.response import Response
 
 from users.serializers.users import UserSerializer
-from users.models import User, SubscriptionPlan
+from users.models import User, SubscriptionPlan, PinnedChat
 from django.views.generic import TemplateView
 
 
@@ -43,3 +43,33 @@ class UserView(views.APIView):
         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
 
+class PinnedChatView(APIView):
+    def get(self, request, pinned_id=None):
+        if pinned_id:
+            pinned = get_object_or_404(PinnedChat, id=pinned_id)
+            serializer = PinnedChatserializer(instance=pinned)
+        else:
+            pinneds = PinnedChat.objects.all()
+            serializer = PinnedChatserializer(instance=pinneds, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, requset):
+        serializer = PinnedChatserializer(data=requset.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({}, status=status.HTTP_201_CREATED)
+
+    def put(self, request, pinned_id):
+        pinned = get_object_or_404(PinnedChat, id=pinned_id)
+        data = request.data
+        serializer = PinnedChatserializer(instance=pinned, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({}, status=status.HTTP_200_OK)
+
+    def delete(self, request, pinned_id):
+        pinned = PinnedChat.objects.get(id=pinned_id)
+        pinned.deleted_at = timezone.now()
+        pinned.save()
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
