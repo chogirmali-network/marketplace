b042772855ecc436e6b1cee37b7827ae7004a344
Jahongir Ibragimov <118691897+ChogirmaliYigit@users.noreply.github.com>
2023-07-28 11:26:04 +0500

    update: pinned chat

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from users.views.users import UserView
+from users.views.users import UserView, PinnedChatView
 
 
 urlpatterns = [
     path('users', UserView.as_view(), name='users'),
     path('user/<int:user_id>', UserView.as_view(), name='user-detail'),
+    path("pinneds", PinnedChatView.as_view(), name="pinned-list"),
+    path("pinned/<int:pinned_id/>", PinnedChatView.as_view(), name="pinned-detail"),
 ]
