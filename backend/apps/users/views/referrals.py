1b97b8f50f750b755eaa277292c95036be82cd9a
Jahongir Ibragimov <118691897+ChogirmaliYigit@users.noreply.github.com>
2023-08-28 17:51:51 +0500

    Update referrals.py

--- backend/apps/users/views/referrals.py
+++ backend/apps/users/views/referrals.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from backend.apps.users.serializers.referrals import ReferralSerializer
+from users.serializers.referrals import ReferralSerializer
 
 
 class SignUpWithReferralView(APIView):
