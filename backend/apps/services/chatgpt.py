de945f07c0ff0434cbe593abe17132fc9192894c
ChogirmaliYigit <chogirmali.yigit@gmail.com>
2023-10-11 16:08:29 +0500

    fix: services (chatgpt)

---
+++ backend/apps/services/chatgpt.py
@@ -0,0 +1,29 @@
+import requests
+import os
+
+from services.models import ChatGPT
+
+
+def chat_gpt3(prompt, user):
+    api_key = os.getenv("OPENAI_API_KEY")
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}',
+    }
+    data = {
+        'prompt': prompt,
+        'max_tokens': 60,
+    }
+    response = requests.post('https://api.openai.com/v1/engines/davinci-codex/completions', headers=headers, json=data)
+    response_json = response.json()
+    return {
+        'content': response_json['choices'][-1]['message']['content'],
+        'role': response_json['choices'][-1]['message']['role'],
+        'user': user,
+    }
+
+
+def save_chat_gpt3(chat_gpt3_dict):
+    chat_gpt = ChatGPT(**chat_gpt3_dict)
+    chat_gpt.save()
+    return chat_gpt
